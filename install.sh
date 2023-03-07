4bd583415b5794ee665a76eb70b86e7fc3ddaedc
Daniel <daniel@technikamateur.de>
2023-03-07 12:25:30 +0100

    many changes

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BIN_PATH=~/.local/bin/vcs-downloader
-mkdir ~/.local
-mkdir ~/.local/bin
-echo $'#!/bin/bash\npython3 '$PWD'/vcs-downloader.py $1 $2' > $BIN_PATH
-chmod +x $BIN_PATH 
-echo "Installed vcs-downloader successfully"
--- vcs-downloader.py
+++ vcs-downloader.py
@@ -1,53 +1,75 @@
 import sys
 import urllib.request
-import re
 import ssl
+from joblib import Parallel, delayed
+import subprocess
+import os
+
+ts_list = list()
+context = ssl._create_unverified_context()
+phpSessId = "abjkl556"
+prefix = ""
+
+
+def download_part(file):
+    context = ssl._create_unverified_context()
+    url = prefix + "/" + file
+    try:
+        with open(file, "wb") as ts_file:
+            request = urllib.request.Request(url)
+            # Add login Cookie
+            if (phpSessId):
+                request.add_header("Cookie", "PHPSESSID=" + phpSessId)
+            response = urllib.request.urlopen(request, context=context)
+            ts_file.write(response.read())
+    except urllib.error.HTTPError as e:
+        print('HTTP Error at', url, '| Code:', e.code)
+    return
+
 
 if (len(sys.argv) > 1 and sys.argv[1]):
     downloadUrl = sys.argv[1]
 else:
-    downloadUrl = input("Please enter the Download URL: ")
-downloadUrl = re.sub("_[0-9]{3}.ts", "", downloadUrl)
+    downloadUrl = input("Please enter the Download URL of m3u file: ")
+
+print("Parsing your link...")
+prefix = downloadUrl.rsplit("/", 1)[0]
+
+with open("playlist.m3u", 'wb') as m3u_file:
+    try:
+        request = urllib.request.Request(downloadUrl)
+        request.add_header("Cookie", "PHPSESSID=" + phpSessId)
+        response = urllib.request.urlopen(request, context=context)
+        m3u_file.write(response.read())
+    except urllib.error.HTTPError as e:
+        print('HTTP Error at', downloadUrl, '| Code:', e.code)
+
+with open("playlist.m3u", 'r') as m3u_file:
+    line = m3u_file.readline().rstrip()
+    while line:
+        if line.endswith(".ts"):
+            ts_list.append(line)
+        line = m3u_file.readline().rstrip()
+
+print(f"File consists of {len(ts_list)} parts.\n")
 
 if (len(sys.argv) > 2 and sys.argv[2]):
-    filename = sys.argv[2] + ".ts"
+    filename = sys.argv[2]
 else:
-    filename = input("File Name? (No ext and special chars): ") + ".ts"
-print("Downloading from " + downloadUrl)
+    filename = input("Output name: ")
 
-filesize = 100
-i = 0
-videodata = ""
-context = ssl._create_unverified_context()
-phpSessId = None
-while filesize > 0:
-    numberStr = str(i)
-    if i < 10000:
-        numberStr = numberStr.zfill(3)
-    url = downloadUrl + "_"+ numberStr +".ts"
-    localFile = numberStr + ".ts"
-    #print(url)
-    print('.', end='',flush=True)
-    #urllib.request.urlretrieve(url, localFile)
-    try:
-        request = urllib.request.Request(url)
-        # Add login Cookie
-        if (phpSessId):
-            request.add_header("Cookie", "PHPSESSID=" + phpSessId)
-        response = urllib.request.urlopen(request, context=context)
-        ts = response.read()
-        filesize = len(ts)
-        tsfile = open(filename, 'ab')
-        tsfile.write(ts)
-        #filesize = os.stat(localFile).st_size
-        i = i + 1
-    except urllib.error.HTTPError as e:
-        if (e.code == 404):
-            if (i < 10):
-                phpSessId = input("Please enter the PHPSESSID Cookie: ")
-            else:
-                # Pretend filesize is 0 to exit loop
-                filesize = 0
-        else:
-            print('HTTP Error at', url, '| Code:', e.code)
-print('Done downloading ' + localFile + '!')
+print("Starting download. Might take some time...")
+#Parallel(n_jobs=16)(delayed(download_part)(file) for file in ts_list)
+
+with open("ts_file.txt", "w", encoding='utf-8') as ts_file:
+    for line in ts_list:
+        ts_file.write(f"file {line}\n")
+
+ffmpeg = ["ffmpeg", "-f", "concat", "-vaapi_device", "/dev/dri/renderD128", "-i", "ts_file.txt", "-vf", 'format=nv12,hwupload', "-map", "0", "-c:a", "copy", "-c:v", "hevc_vaapi", "-crf", "25", "-preset", "slow", filename]
+subprocess.run(ffmpeg)
+
+# clean up
+#dir_list = os.listdir(".")
+#or file in dir_list:
+#    if file.endswith(".ts"):
+ #       os.remove(file)
